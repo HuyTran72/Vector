716fd559fef776cb31bb9d2a88ad2f5ce4a93fee
Huy Tran <baohuy82.th@gmail.com>
2023-09-07 11:41:21 -0700

    up2

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(4, 5, 6);
+        System.out.println("Value of vector 1 are: ");
+        System.out.println("x => %f, y => %f, z => %f\n", v1.getX(), v1.getY(), v1.getZ());
+        System.out.println("Magnitude of vector 1: %f\n", v1.computeMagnitude());
+
+        System.out.println();
+
+        System.out.println("Values of vector 2 are: ");
+        
+    }
 }
