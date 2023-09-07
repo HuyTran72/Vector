848fa11fcc6cc1ce69480a17ac6c68352c85875c
Huy Tran <baohuy82.th@gmail.com>
2023-09-07 12:00:35 -0700

    up4

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ public class Main {
         Vector v1 = new Vector(1, 2, 3);
         Vector v2 = new Vector(4, 5, 6);
         System.out.println("Value of vector 1 are: ");
-        System.out.println("x => %f, y => %f, z => %f\n", v1.getX(), v1.getY(), v1.getZ());
-        System.out.println("Magnitude of vector 1: %f\n", v1.computeMagnitude());
+        System.out.printf("x = %f, y = %f, z = %f\n", v1.getX(), v1.getY(), v1.getZ());
+        System.out.printf("Magnitude of vector 1: %f\n", v1.computeMagnitude());
 
         System.out.println();
 
@@ -17,7 +17,7 @@ public class Main {
         System.out.println("Adding vector 2 to vector 1...");
         v1.add(v2);
         System.out.println("Values of vector 1 are: ");
-        System.out.printf("x=> %f, y=> %f, z=> %f\n", v1.getX(), v1.getY(), v1.getZ());
+        System.out.printf("x = %f, y = %f, z = %f\n", v1.getX(), v1.getY(), v1.getZ());
         System.out.printf("Magnitude of vector 1: %f\n", v1.computeMagnitude());
 
         System.out.println();
@@ -25,7 +25,7 @@ public class Main {
         System.out.println("Normalising vector 2...");
         v2.normalize();
         System.out.println("Values of vector 2 are: ");
-        System.out.printf("x=> %f, y=> %f, z=> %f\n", v2.getX(), v2.getY(), v2.getZ());
+        System.out.printf("x = %f, y = %f, z = %f\n", v2.getX(), v2.getY(), v2.getZ());
         System.out.printf("Magnitude of vector 2: %f\n", v2.computeMagnitude());
 
         System.out.println();
@@ -33,7 +33,7 @@ public class Main {
         System.out.println("Scaling vector 2 by 5.4");
         v2.scaleVector(5.4);
         System.out.println("Values of vector 2 are: ");
-        System.out.printf("x=> %f, y=> %f, z=> %f\n", v2.getX(), v2.getY(), v2.getZ());
+        System.out.printf("x = %f, y = %f, z = %f\n", v2.getX(), v2.getY(), v2.getZ());
         System.out.printf("Magnitude of vector 2: %f\n", v2.computeMagnitude());
 
         System.out.println();
